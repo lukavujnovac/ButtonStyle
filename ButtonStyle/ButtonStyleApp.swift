9fe4831cf4aad7c2efecf013e9d4bb3645b3b745
Luka Vujnovac <84441240+lukavujnovac@users.noreply.github.com>
2024-01-20 15:58:16 +0100

    Initial Commit

---
+++ ButtonStyle/ButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStyleApp.swift
+//  ButtonStyle
+//
+//  Created by Luka Vujnovac on 20.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct ButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
